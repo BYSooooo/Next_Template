7ccb4453abf3c739ae2326f8adad2f002cc8485d
BYSooooo <avera0128@gmail.com>
2023-07-15 15:04:52 +0900

    [230715] Stop Edit Toggle Drawer Component

--- next_template/src/movie/components/FilterBtn.tsx
+++ next_template/src/movie/components/FilterBtn.tsx
@@ -3,14 +3,29 @@ import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Box, SwipeableDrawer } from '@mui/material';
-import ToggleDrawer from './ToggleDrawer';
-
-
-
 
 export default function FilterBtn() {
 
-    const filterBar = (anchor : string) => (
+    const [state, setState] = React.useState({
+        top : false, left : false, bottom : false, right : false
+    })
+
+    const toggleDrawer = (anchor: string, open: boolean) => (
+        event : React.KeyboardEvent | React.MouseEvent
+    ) => {
+        if( event && 
+            event.type === 'keydown' &&
+            (   
+                (event as React.KeyboardEvent).type === 'Tab' ||
+                (event as React.KeyboardEvent).type === 'Shift'
+            ))
+        { return; }
+         setState({...state, [anchor] : open})
+    }
+
+
+
+    const filterBar = () => (
         <Box sx={{width : "100%"}}
             role="presentation">
         <Typography>
@@ -18,25 +33,25 @@ export default function FilterBtn() {
         </Typography>
         </Box>
     )
-    
 
     return (
         <>
-            <Button sx={{ width: "100%", height : "100%" }} onClick={ToggleDrawer('bottom', true)}>
+            <Button sx={{ width: "100%", height : "100%" }} onClick={toggleDrawer('bottom', true)} >
                 <KeyboardArrowDown />
                 <Typography>
                     Filter
                 </Typography>
             </Button>
-            <React.Fragment>
+            <React.Fragment>    
                 <SwipeableDrawer
                     anchor="bottom"
-                    open={false}
-                    onClose={ToggleDrawer('bottom', false)}
-                    onOpen={ToggleDrawer('bottom',true)}>
-                {filterBar('bottom')}
+                    open={state['bottom']}
+                    onClose={toggleDrawer('bottom', false)}
+                    onOpen={toggleDrawer('bottom', true)}>
+                    {filterBar()}
                 </SwipeableDrawer>
             </React.Fragment>
+            
         </>           
     )
 }
--- next_template/src/movie/components/ToggleDrawer.tsx
+++ next_template/src/movie/components/ToggleDrawer.tsx
@@ -1,26 +1,36 @@
+import { SwipeableDrawer } from "@mui/material";
 import * as React from "react";
 
-export default function ToggleDrawer(anchor : string, open : boolean) {
-    
+/**
+ * Now Working Component
+ * @param param0 
+ * @returns 
+ */
+export default function ToggleDrawer({anchor, component} : {anchor : 'top' | 'left' | 'bottom' | 'right' , component : React.JSX.Element}) {
+
     const [state, setState] = React.useState({
-        top : false,
-        left : false,
-        bottom : false,
-        right : false
+        top : false, left : false,bottom : false, right : false
     })
-
-    return function (event : React.KeyboardEvent | React.MouseEvent ) {
-
+    return function (event : React.KeyboardEvent | React.MouseEvent) {
         if( event && 
             event.type === 'keydown' &&
             (   
                 (event as React.KeyboardEvent).type === 'Tab' ||
                 (event as React.KeyboardEvent).type === 'Shift'
-            )
-        )
-        {
-            return;
-        }
+            ))
+        { return; }
          setState({...state, [anchor] : open})
-    }
+         console.log(event)
+        return (
+            <React.Fragment>
+            <SwipeableDrawer
+                anchor={anchor}
+                open={state[anchor]}
+                onClose={this(anchor, false)}
+                onOpen={this(anchor, true)}>
+                {component}
+            </SwipeableDrawer>
+        </React.Fragment>
+        )
+    }        
 }
