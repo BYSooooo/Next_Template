735a4b4fd1cc83eb0d78af9fc00082d3f73a06c8
BYSooooo <avera0128@gmail.com>
2023-07-18 01:12:16 +0900

    [230718] Edit Rate Setting Slider in SearchFilter

--- next_template/src/movie/components/SearchFilter.tsx
+++ next_template/src/movie/components/SearchFilter.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { delSelectedGenre, setSelectedGenre } from "@/redux/features/movieReducer";
 import { useAppDispatch, useAppSelector } from "@/redux/hook";
-import { Box, Chip, Typography } from "@mui/material";
+import { Box, Chip, FormControlLabel, Slider, Stack, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 
 import DoneIcon from '@mui/icons-material/Done'
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import dayjs, { Dayjs } from "dayjs";
+import Checkbox from "@mui/material/Checkbox";
 
 export default function SearchFilter() {
     const genreList : MovieGenreInfo[] = useAppSelector((state) => state.movieGenre);
@@ -34,15 +35,22 @@ export default function SearchFilter() {
 
     const [fromDate, setFromDate] = React.useState<Dayjs | null>(dayjs(new Date()))
     const [toDate, setToDate] = React.useState<Dayjs | null>(dayjs(new Date()))
+    
+    const [allGenre, setAllGenre] = React.useState(true);
+    const [allDate, setAllDate] = React.useState(true);
+    const [allRate, setAllRate] = React.useState(true);
 
     console.log(selGenreList)
     return (
         <Box sx={{ width : "100%"}} >
             <Grid container direction='row'>
-                <Grid container direction='column' sx={{width : "45%", m: 1}}>    
-                    <Typography>
-                        Genre
-                    </Typography>
+                <Grid container direction='column' sx={{width : "45%", m: 0.5}}>    
+                    <Grid container direction='row' alignItems='Center' sx={{pl : "1rem"}}>
+                        <Typography sx={{ mr: "1rem"}}> 
+                            Genre
+                        </Typography>
+                        <FormControlLabel control={<Checkbox checked={allGenre} onChange={()=> setAllGenre(!allGenre)} />} label="All"/>
+                    </Grid>
                     <Grid direction='row'>
                         <Box sx={{ m: 1, p : 1, border : '1px solid gray', borderRadius : "1rem"}}>
                             {genreList.map((genre : MovieGenreInfo) => {
@@ -53,41 +61,41 @@ export default function SearchFilter() {
                                         label={genre.name} 
                                         onClick={() => onClickAddGenre(genre)}
                                         color={checkSelList(genre.id) === true ? "primary" : "default" }
+                                        disabled={allGenre}
                                     />
                                 )
                             })}
                         </Box>
                     </Grid>
                 </Grid>
-                <Grid container direction='column' sx={{width : "25%", m: 1, minWidth : "10rem"}}>
-                    <Typography>
-                        Release Date
-                    </Typography>
+                <Grid container direction='column' sx={{width : "30%", m: 0.5, minWidth : "10rem"}}>
+                    <Grid container direction='row' alignItems='Center' sx={{pl : "1rem"}}>
+                        <Typography sx={{ mr: "1rem"}}>
+                            Release Date
+                        </Typography>
+                        <FormControlLabel control={<Checkbox checked={allDate} onChange={()=> setAllDate(!allDate)} />} label="All"/>     
+                    </Grid>
                     <Box sx={{ width : "80%", m: 1, p: 1, border: '1px solid gray', borderRadius : "1rem"}}>
                         <Grid container direction='column'>
                             <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                <DatePicker label="From Date" value={fromDate} onChange={(newValue) => setFromDate(newValue)} sx={{m : "0.5rem"}}/>
-                                <DatePicker label="To Date" value={toDate} onChange={(newValue) => setToDate(newValue)} sx={{m : "0.5rem"}}/> 
+                                <DatePicker label="From Date" value={fromDate} onChange={(newValue) => setFromDate(newValue)} sx={{m : "0.5rem"}} disabled={allDate}/>
+                                <DatePicker label="To Date" value={toDate} onChange={(newValue) => setToDate(newValue)} sx={{m : "0.5rem"}} disabled={allDate} /> 
                             </LocalizationProvider>
                         </Grid>
                     </Box>
                 </Grid>
-                <Grid container direction='column' sx={{width : "20%", m: 1, minWidth: "10rem"}}>
-                    <Typography>
-                        Rate
-                    </Typography>
-                    <Box sx={{ width : "80%", m: 1, p: 1, border: '1px solid gray', borderRadius : "1rem"}}>
-
+                <Grid container direction='column' sx={{width : "20%", m: 0.5, minWidth: "10rem"}}>
+                    <Grid container direction='row' alignItems='Center' sx={{pl : "1rem"}}>
+                        <Typography sx={{ mr: "1rem"}}> 
+                            Rate
+                        </Typography>
+                        <FormControlLabel control={<Checkbox checked={allRate} onChange={()=> setAllRate(!allRate)} />} label="All"/>
+                    </Grid>
+                    <Box justifyItems="stretch" sx={{ width : "80%", m: 1, p: 1, border: '1px solid gray', borderRadius : "1rem"}}>
+                        <Slider valueLabelDisplay="auto" marks step={1} min={0} max={5} sx={{ width: "80%"}} disabled={allRate}/>
                     </Box>
                 </Grid>
-                
-            
-            
-            
-            
             </Grid>
-            
-
         </Box>
     )
 }
