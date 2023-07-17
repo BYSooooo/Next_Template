e32606d56781d69f409a0760a449876d0d345471
BYSooooo <avera0128@gmail.com>
2023-07-17 23:32:34 +0900

    [230717] Add FromTo DatePicker in SearchFilter

--- next_template/src/movie/components/SearchFilter.tsx
+++ next_template/src/movie/components/SearchFilter.tsx
@@ -1,8 +1,13 @@
+import React from "react";
 import { delSelectedGenre, setSelectedGenre } from "@/redux/features/movieReducer";
 import { useAppDispatch, useAppSelector } from "@/redux/hook";
 import { Box, Chip, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
-import React from "react";
+
+import DoneIcon from '@mui/icons-material/Done'
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import dayjs, { Dayjs } from "dayjs";
 
 export default function SearchFilter() {
     const genreList : MovieGenreInfo[] = useAppSelector((state) => state.movieGenre);
@@ -10,42 +15,79 @@ export default function SearchFilter() {
     const dispatch = useAppDispatch();
 
     function onClickAddGenre(genre : MovieGenreInfo){
-        dispatch(setSelectedGenre(genre))    
-        console.log(genre)
+        /** if Genre not selected, add Store */
+        if(selGenreList.findIndex(
+            (selected) => selected.id === genre.id) === -1) {
+            dispatch(setSelectedGenre(genre))
+        /** if Genre seleted already, delete in Store*/    
+        } else {
+            dispatch(delSelectedGenre(genre.id))
+        }
+        
     }
-    const onClickDelGenre = (genreId : number) => {
-        dispatch(delSelectedGenre(genreId))        
-    }
+
     const checkSelList = (genreId : number) => {
         const check = selGenreList.findIndex((genre) => genre.id === genreId)
         return check === -1 ? false : true ;
         
     }
 
+    const [fromDate, setFromDate] = React.useState<Dayjs | null>(dayjs(new Date()))
+    const [toDate, setToDate] = React.useState<Dayjs | null>(dayjs(new Date()))
+
     console.log(selGenreList)
     return (
         <Box sx={{ width : "100%"}} >
-            <Grid container direction='column'>
-                <Box sx={{ paddingInline : "2rem"}}>
+            <Grid container direction='row'>
+                <Grid container direction='column' sx={{width : "45%", m: 1}}>    
                     <Typography>
                         Genre
                     </Typography>
                     <Grid direction='row'>
-                        {genreList.map((genre : MovieGenreInfo) => {
-                            return (
-                                <Chip
-                                    sx={{ margin : "0.3rem" }} 
-                                    label={genre.name} 
-                                    onClick={() => onClickAddGenre(genre)}
-                                    disabled={checkSelList(genre.id)}
-                                />
-                            )
-                        })}
+                        <Box sx={{ m: 1, p : 1, border : '1px solid gray', borderRadius : "1rem"}}>
+                            {genreList.map((genre : MovieGenreInfo) => {
+                                return (
+                                    <Chip
+                                        icon={checkSelList(genre.id) === true ? <DoneIcon /> : null}
+                                        sx={{ margin : "0.3rem" }} 
+                                        label={genre.name} 
+                                        onClick={() => onClickAddGenre(genre)}
+                                        color={checkSelList(genre.id) === true ? "primary" : "default" }
+                                    />
+                                )
+                            })}
+                        </Box>
                     </Grid>
-                </Box>
-                
+                </Grid>
+                <Grid container direction='column' sx={{width : "25%", m: 1, minWidth : "10rem"}}>
+                    <Typography>
+                        Release Date
+                    </Typography>
+                    <Box sx={{ width : "80%", m: 1, p: 1, border: '1px solid gray', borderRadius : "1rem"}}>
+                        <Grid container direction='column'>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker label="From Date" value={fromDate} onChange={(newValue) => setFromDate(newValue)} sx={{m : "0.5rem"}}/>
+                                <DatePicker label="To Date" value={toDate} onChange={(newValue) => setToDate(newValue)} sx={{m : "0.5rem"}}/> 
+                            </LocalizationProvider>
+                        </Grid>
+                    </Box>
+                </Grid>
+                <Grid container direction='column' sx={{width : "20%", m: 1, minWidth: "10rem"}}>
+                    <Typography>
+                        Rate
+                    </Typography>
+                    <Box sx={{ width : "80%", m: 1, p: 1, border: '1px solid gray', borderRadius : "1rem"}}>
 
+                    </Box>
+                </Grid>
+                
+            
+            
+            
+            
             </Grid>
+            
+
         </Box>
     )
 }
