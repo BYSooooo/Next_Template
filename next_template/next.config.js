855eda813acdd2cd8eb8f8d2c49db23359d9b637
BYSooooo <avera0128@gmail.com>
2023-10-03 21:25:35 +0900

    [231003] Test

--- next_template/next.config.js
+++ next_template/next.config.js
@@ -3,8 +3,6 @@
 const API_KEY = process.env.API_KEY
 
 const nextConfig = {
-    basePath : process.env.NEXT_PUBLIC_BASE_PATH,
-    assetPrefix : process.env.NEXT_PUBLIC_BASE_PATH,
     reactStrictMode: false,
     swcMinify : true,
     modularizeImports : {
