30f242d839aff760b61d5e24197f6144eaedccd8
BYSooooo <avera0128@gmail.com>
2023-10-05 19:39:54 +0900

    [231005] Test Deploy

--- next_template/next.config.js
+++ next_template/next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
             },
             {
                 source : "/api/movies/search/:query",
-                destination : `https://api.themoviedb.org/3/search/movie:query?api_key=${API_KEY}`
+                destination : `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}:query`
             },
             {
                 source : "/api/movies/detail/:query",
