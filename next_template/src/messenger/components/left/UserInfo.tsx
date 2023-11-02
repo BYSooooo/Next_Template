0dccc6882b08f95c698e1e80e51e072e294e1991
BYSooooo <avera0128@gmail.com>
2023-11-02 21:52:30 +0900

    [231102] In Development Messenger Page

--- next_template/src/messenger/components/left/UserInfo.tsx
+++ next_template/src/messenger/components/left/UserInfo.tsx
@@ -1,26 +1,13 @@
 import React, { HTMLAttributes } from 'react';
 import { firebaseAuth } from '../../../../firebaseConfig';
-import { Icon_Check_NotVeri, Icon_Check_Verified } from '@/messenger/styles/IconPack';
+import { CheckIcon, UserIcon } from '@heroicons/react/20/solid';
 
 export default function UserInfo({pageControl} : {pageControl : Function}) {
     
     const userInfo = firebaseAuth.currentUser;
     console.log(userInfo)
 
-    const avatarImage = ()=> {
-        const customClass : HTMLAttributes<HTMLDivElement> = {
-            
-        }
-        //https://flowbite.com/docs/components/avatar/
-        return (
-            <div className='relative w-32 h-32 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600'>
-                <svg className="absolute w-36 h-36 text-gray-400 -left-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd">
-                    </path>
-                </svg>
-            </div>
-        )
-    }
+
     const setDisplayName = () => {
         let dName = ""
         if(dName) {
@@ -37,17 +24,14 @@ export default function UserInfo({pageControl} : {pageControl : Function}) {
             <div className='flex rounded-full w-36 h-36 border-solid border-gray-400 border-2 items-center justify-center'>
                 {firebaseAuth.currentUser.photoURL 
                 ? <img src={firebaseAuth.currentUser.photoURL} /> 
-                : avatarImage() }
+                : <UserIcon className='w-auto h-auto text-gray-400'/> }
+            </div>
+            <div className='absoulte justify-end'>
+                <button onClick={()=>pageControl("Profile")}>
+                    Edit
+                </button>
             </div>
             <div>
-                <div className='absolute flext justify-end'>
-                    <button 
-                        onClick={()=>pageControl("Profile")}>
-                        Edit
-                    </button>
-
-                </div>
-                
                 <h3 className='text-md text-gray-500'>
                     Name
                 </h3>
@@ -61,12 +45,9 @@ export default function UserInfo({pageControl} : {pageControl : Function}) {
                     <h6 className='text-lg font-bold'>
                         {firebaseAuth.currentUser.email} 
                     </h6>
-                    {
-                        userInfo.emailVerified 
-                        ? <Icon_Check_Verified/> 
-                        : <Icon_Check_NotVeri />
-                    }
-                    
+                    {userInfo.emailVerified 
+                    ? <CheckIcon className='w-6 h-6 text-green-500'/> 
+                    : <CheckIcon className='w-6 h-6 text-red-500'/> }
                 </div>
                 <h3 className='text-md text-gray-500'>
                     Introduce
