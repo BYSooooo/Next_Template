1a2cec511b095a323aafbbe7eb624beaa3f78f7c
BYSooooo <avera0128@gmail.com>
2023-11-22 14:02:51 +0900

    [231122] Deploy Test

--- next_template/src/messenger/layouts/MainAuth.tsx
+++ next_template/src/messenger/layouts/MainAuth.tsx
@@ -24,25 +24,25 @@ export default function MainAuth() {
                 </h6>
             </div>
             <div className="divide-y-2 divide-solid">
-                <div className='grid p-3 h-52 mt-10 grid-rows-3 py-6 gap-2'>
-                    <div className="flex justify-center items-center min-w-full">
+                <div className='p-3 h-52 mt-10 py-6'>
+                    <div className="flex justify-center items-center my-3">
                         <button
                             onClick={()=>callAuth("Google")}
-                            className=" w-2/3 rounded-full border-solid border-blue-500 border-2  p-2 hover:bg-blue-500">
+                            className=" w-60 rounded-full border-solid border-blue-500 border-2  p-2 hover:bg-blue-500">
                             Sign in with Google
                         </button>
                     </div>
-                    <div className="flex justify-center items-center min-w-full">
+                    <div className="flex justify-center items-center my-3">
                         <button
                             onClick={()=> callAuth("Github")}
-                            className="w-2/3 rounded-full border-solid border-gray-800 border-2 p-2 hover:bg-gray-800 hover:text-white" >
+                            className="w-60 rounded-full border-solid border-gray-800 border-2 p-2 hover:bg-gray-800 hover:text-white" >
                             Sign in with GitHub
                         </button>
                     </div>
-                    <div className="flex justify-center items-center min-w-full">
+                    <div className="flex justify-center items-center my-3">
                         <button
                             onClick={()=> setShowModal(true)}
-                            className="w-2/3 rounded-full border-solid  border-gray-400 border-2 p-2 hover:bg-gray-400">
+                            className="w-60 rounded-full border-solid  border-gray-400 border-2 p-2 hover:bg-gray-400">
                             Sign in with Email
                         </button>
                     </div>
