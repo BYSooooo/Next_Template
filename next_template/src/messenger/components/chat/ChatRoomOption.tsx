0cfab3c219bb8ea9c470c6f830c6658e874212f6
BYSooooo <avera0128@gmail.com>
2024-01-19 23:49:15 +0900

    [240119]In Development Chat Room Option

--- next_template/src/messenger/components/chat/ChatRoomOption.tsx
+++ next_template/src/messenger/components/chat/ChatRoomOption.tsx
@@ -3,17 +3,41 @@ import React from 'react';
 import { ChatRoomMenu } from './ChatRoomMenu';
 import { useAppDispatch, useAppSelector } from '@/redux/hook';
 import { setPageRendering } from '@/redux/features/messengerReducer';
+import { getSelectedChatInfo } from '../FirebaseController';
+import { ChatRoomInfo, MessageInfo } from '../../../../msg_typeDef';
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { firebaseStore } from '../../../../firebaseConfig';
 
 export function ChatRoomOption() {
+    const [messages, setMessages] = React.useState<MessageInfo[]>([])
+
     const chatRoomReducer = useAppSelector((state)=> state.messengerCurChatInfo);
     const currentUserInfo = useAppSelector((state)=> state.messengerCurUserInfo);
-
     const dispatch = useAppDispatch()
+
+    React.useEffect(()=> {
+        getMessagesList()
+    },[])
+
     const onClickHandler = (select:string)=> {
         if(select === "ok") {
             dispatch(setPageRendering({middle : "ChatRoom"}))
         }
     }
+
+    const getMessagesList = async()=> {
+        const colRef = query(collection(firebaseStore,`chatList/${chatRoomReducer.uuid}/messages`),orderBy('createDate','asc'));
+        const resultArray : MessageInfo[] = []
+        await getDocs(colRef).then((items)=> {
+            items.forEach((message)=> {
+                const item = message.data() as MessageInfo
+                {item.attachedYn === true && resultArray.push(item)}
+                
+            })
+        })
+        setMessages(resultArray)
+    }
+
 
     return (
         <div className='w-fit border-2 border-solid border-gray-500 rounded-md p-2 m-2'>
@@ -23,6 +47,26 @@ export function ChatRoomOption() {
                 </h4>
                 <ChatRoomMenu />
             </div>
+            <div className='p-1'>
+                <h4>
+                    Attached File
+                </h4>
+            </div>
+            <div className='grid grid-cols-3 gap-3 overflow-y-scroll px-2'>
+                {messages.map((item)=> {
+                    return (
+                        <img src={item.attachedValue} className='rounded-md w-20 h-20'/>
+                    )
+                })}
+            </div>
+            <div className='p-1'>
+                <h4>
+                    Messages Export
+                </h4>
+            </div>
+            <button className='w-full rounded-lg border-2 border-solid border-green-600'>
+                Export
+            </button>
             <div>
                 <button onClick={()=>onClickHandler("ok")}>
                     return
