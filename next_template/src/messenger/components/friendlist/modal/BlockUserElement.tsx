d7691600d4f601197b603e31bf9c12d8127b3c40
BYSooooo <avera0128@gmail.com>
2024-02-26 23:16:44 +0900

    [240226]In Dev Block User Manage

--- next_template/src/messenger/components/friendlist/modal/BlockUserElement.tsx
+++ next_template/src/messenger/components/friendlist/modal/BlockUserElement.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { getUserInfo } from '../../FirebaseController';
 import { UserInfo } from '../../../../../msg_typeDef';
+import { ArrowRightOnRectangleIcon } from '@heroicons/react/20/solid';
 
 export default function BlockUserElement({email} : {email: string}) {
     const [blockUser, setBlockUser] = React.useState<UserInfo>(); 
@@ -16,13 +17,18 @@ export default function BlockUserElement({email} : {email: string}) {
     }
 
     return (blockUser && 
-        <li className='border-2 border-gray-200'>
-            <div>
-                {blockUser.email}
-            </div>
-            <div>
-                {blockUser.uid }
-            </div>
+        <li className='flex p-2 m-1 rounded-md border-2 border-slate-500 hover:text-white hover:cursor-pointer transition duration-200 '>
+            
+                {blockUser.photoURL 
+                    ? <img src={blockUser.photoURL} />
+                    : <h4> Photo is null</h4>
+                }
+                <h4 className='text-xs'>
+                    {blockUser.email}
+                </h4>
+                <ArrowRightOnRectangleIcon className='w-4 h-4' />
+            
+            
         </li>
     )
 }
