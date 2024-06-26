8357c73b3441412f3d3fdba05cc10a4db0b025fb
BYSooooo <avera0128@gmail.com>
2024-06-26 23:14:13 +0900

    [240626]Connect Database, In Defining Table

--- next_template/src/app/shopping/page.tsx
+++ next_template/src/app/shopping/page.tsx
@@ -1,18 +1,15 @@
 "use client"
 import React from 'react';
 
-import { onClickTest } from "@/shopping/prisma/test"
-
-
 export default function ShoppingPage() {
     React.useEffect(()=> {
         
     },[])
 
     return (
-        <div className="flex container m-20 mx-auto h-auto justify-center">
+        <div className="flex container mx-auto h-auto justify-center">
             <div>
-                <button onClick={onClickTest}>
+                <button>
                     <h1>
                         Hello
                     </h1>
