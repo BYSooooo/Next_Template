236d991d8438082dc5c12f543dda8720a80918b6
BYSooooo <avera0128@gmail.com>
2024-07-25 23:54:34 +0900

    [240725]In Dev Dialog

--- next_movie/src/dialog/content/Overview.tsx
+++ next_movie/src/dialog/content/Overview.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, DialogActions, DialogContent, DialogTitle, Skeleton, Typography } from "@mui/material";
+import { Box, Button, Chip, DialogActions, DialogContent, DialogTitle, Skeleton, Typography } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { controlDialog } from "../../redux/features";
+import { CalendarMonth } from "@mui/icons-material";
 
 export default function Overview() {
     const dispatch = useAppDispatch()
@@ -18,26 +19,19 @@ export default function Overview() {
                 <Box sx={{
                     display : 'flex',
                     borderRadius : 4 }}>
-                    { movieSlice.poster_path 
-                        ? <img 
-                            style={{
-                                borderRadius : 5,
-                                width : "7rem",
-                            }}
-                            alt={movieSlice.original_title}
-                            src={`https://image.tmdb.org/t/p/w500${movieSlice.poster_path}&fit=crop`}/>
-                        : <Skeleton />
-                    }
-                    
-                    <Box sx={{
-                        mx : 3
-                    }}>
-                        <Typography sx={{ }}>
+                    <img 
+                        style={{
+                            borderRadius : 5,
+                            width : "8rem",
+                        }}
+                        alt={"Loading..."}
+                        src={`https://image.tmdb.org/t/p/w500${movieSlice.poster_path}&fit=crop`}/>                    
+                    <Box sx={{ mx : 3}} display={"inline-block"} width="20vw">
+                        <Typography variant="h6" fontWeight="bold" noWrap={true}>
                             {movieSlice.title}
                         </Typography>
-                        <Typography>
-                            {movieSlice.release_date}
-                        </Typography>
+                        <Chip icon={<CalendarMonth />} label={movieSlice.release_date}/>
+
                     </Box>
                 </Box>
             </DialogContent>
