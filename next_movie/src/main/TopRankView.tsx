5533098ccd2a63431534349be3723ade99e86e39
BYSooooo <avera0128@gmail.com>
2024-07-30 23:36:10 +0900

    [240730]In Dev Main Page Concept

--- next_movie/src/main/TopRankView.tsx
+++ next_movie/src/main/TopRankView.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { dark } from '@mui/material/styles/createPalette';
 
-export default function TopRankView({id, sort} : {id: number, sort : "popular" | "topRate" | "upcoming"}) {
+export default function TopRankView({detail, sort} : {detail: movieOverview, sort : "popular" | "topRate" | "upcomming"}) {
 
+    const switcher = ()=> {
+        switch(sort){
+            case 'popular' : 
+
+        }
+    }
 
     return (
-        <div>
-            This is Top Rank
-        </div>
+        <Box 
+            textAlign={'start'} 
+            flexDirection={'row'}
+            sx={{
+                p : 1,
+                width : "100%",
+                height : "30vw", 
+                borderRadius : 3,
+                bgcolor : 'white',
+                }}>
+            <Box sx={{
+                display : 'inline-flex',
+                borderRadius : 4 
+                }}>
+                <img
+                    style={{
+                        borderRadius : 4,
+                        height : "23vw"
+                    }} 
+                    alt={detail.original_title}
+                    src={`https://image.tmdb.org/t/p/w500${detail.poster_path}`} 
+                />
+
+            </Box>
+            {/* <Typography variant='h5'>
+                {detail.title}
+            </Typography> */}
+        </Box>    
     )
 }
