3882ae7548c46103427efaf23e4ebef5892ea30b
BYSooooo <avera0128@gmail.com>
2024-08-13 23:55:51 +0900

    [240813]In Dev Detail Page

--- next_movie/src/detail/DetailInfo.tsx
+++ next_movie/src/detail/DetailInfo.tsx
@@ -8,15 +8,14 @@ import MoviePopper from "../common/popper/MoviePopper";
 
 export default function DetailInfo({theme,path}:{theme: boolean, path : MovieDetail}) {
     const [anchorEl, setAnchorEl]= React.useState<HTMLElement | null>(null)
-    
+    const [anchorElSe, setAnchorElSe] = React.useState<HTMLElement | null>(null)
     const innerBoxStyle = ()=> theme ? grey[800] : grey[300];
     
-    const boxClick =(event : React.MouseEvent<HTMLElement>)=> {
-        console.log(event.currentTarget)    
-        setAnchorEl(event.currentTarget)
-    }
-    const boxClickAway = ()=> setAnchorEl(null)
+    const genreBoxClick = (event : React.MouseEvent<HTMLElement>)=> setAnchorEl(event.currentTarget);
+    const genreBoxClickAway = ()=> setAnchorEl(null)
 
+    const langBoxClick = (event : React.MouseEvent<HTMLElement>)=> setAnchorElSe(event.currentTarget);
+    const langBoxClickAway = ()=> setAnchorElSe(null)
 
 
     return (
@@ -29,8 +28,113 @@ export default function DetailInfo({theme,path}:{theme: boolean, path : MovieDet
                     bgcolor={theme ? grey[900]: grey[100] }
                     borderRadius={4}
                     >
-                    <Box 
-                        sx={{ p : 1}}
+                    <Box flexDirection='column'>
+                        <Box 
+                            sx={{ p : 1 }}
+                            height="50%"
+                            display='flex' 
+                            flexDirection='row'
+                            justifyContent='space-between'>
+                            <Box 
+                                width="20%" 
+                                bgcolor={innerBoxStyle}
+                                borderRadius={4}
+                                sx={{ p : 1}}>
+                                <Typography 
+                                    variant="subtitle2" 
+                                    textAlign='start'
+                                    gutterBottom>
+                                    Release Date
+                                </Typography>
+                                { path.status === "Released" 
+                                    ? <EventAvailable 
+                                        sx={{ 
+                                            color : theme ? green[300] : green[700],
+                                            fontSize : '50px',
+                                        }}/> 
+                                    : <EventBusy 
+                                        sx={{
+                                            color : theme ? red[300] : red[700],
+                                            fontSize : '50px'
+                                        }}/>
+                                }
+                                <Typography fontWeight='bold'>
+                                    {path.release_date}
+                                </Typography>
+                            </Box>
+                            <Box 
+                                width="20%" 
+                                bgcolor={innerBoxStyle}
+                                borderRadius={4}
+                                sx={{ p : 1}}>
+                                <Typography 
+                                    variant="subtitle2" 
+                                    textAlign='start'
+                                    gutterBottom>
+                                    Running Time
+                                </Typography>
+                                <Schedule 
+                                    sx={{
+                                        color : theme ? brown[300] : brown[700],
+                                        fontSize : '50px'
+                                }}/>
+                                <Typography fontWeight='bold'>
+                                    {path.runtime} Min
+                                </Typography>
+                            </Box>
+                            <ClickAwayListener onClickAway={genreBoxClickAway}>
+                                <Box 
+                                    width="20%" 
+                                    bgcolor={innerBoxStyle}
+                                    borderRadius={4}
+                                    onClick={genreBoxClick}
+                                    sx={{ p : 1,}}>
+                                    <Typography 
+                                        variant="subtitle2" 
+                                        textAlign='start'
+                                        gutterBottom>
+                                        Genre
+                                    </Typography>
+                                    <Tag 
+                                        sx={{
+                                            color : theme ? indigo[300] : indigo[700],
+                                            fontSize : '50px'
+                                    }}/>
+                                    <Typography fontWeight='bold'>
+                                        {path.genres.length} Genres
+                                    </Typography>
+                                </Box>
+                            </ClickAwayListener>
+                            <MoviePopper anchorEl={anchorEl} name="Genres" extra={path.genres} />
+                            <ClickAwayListener onClickAway={langBoxClickAway}>
+                                <Box 
+                                    width="20%" 
+                                    bgcolor={innerBoxStyle}
+                                    borderRadius={4}
+                                    onClick={langBoxClick}
+                                    sx={{ p : 1}}>
+                                    <Typography
+                                        variant="subtitle2"
+                                        textAlign='start'
+                                        gutterBottom>
+                                        Spoken Language
+                                    </Typography>
+                                    <Language 
+                                        sx={{
+                                            color : theme ? lightBlue[300] : lightBlue[700],
+                                            fontSize : '50px'
+                                        }} 
+                                    />
+                                    <Typography fontWeight='bold'>
+                                        {path.spoken_languages.length} Languages
+                                    </Typography>
+                                </Box>
+                            </ClickAwayListener>
+                            <MoviePopper anchorEl={anchorElSe} name="Languages" extra={path.spoken_languages}/>
+                        </Box>
+                    </Box>
+                    <Box
+                        sx={{ p : 1 }}
                         height="50%"
                         display='flex' 
                         flexDirection='row'
@@ -39,98 +143,16 @@ export default function DetailInfo({theme,path}:{theme: boolean, path : MovieDet
                             width="20%" 
                             bgcolor={innerBoxStyle}
                             borderRadius={4}
+                            onClick={langBoxClick}
                             sx={{ p : 1}}>
-                            <Typography 
-                                variant="subtitle2" 
+                            <Typography
+                                variant="subtitle2"
                                 textAlign='start'
                                 gutterBottom>
-                                Release Date
-                            </Typography>
-                            { path.status === "Released" 
-                                ? <EventAvailable 
-                                    sx={{ 
-                                        color : theme ? green[300] : green[700],
-                                        fontSize : '50px',
-                                    }}/> 
-                                : <EventBusy 
-                                    sx={{
-                                        color : theme ? red[300] : red[700],
-                                        fontSize : '50px'
-                                    }}/>
-                            }
-                            <Typography fontWeight='bold'>
-                                {path.release_date}
+                                Budget
                             </Typography>
                         </Box>
-                        <Box 
-                            width="20%" 
-                            bgcolor={innerBoxStyle}
-                            borderRadius={4}
-                            sx={{ p : 1}}>
-                            <Typography 
-                                variant="subtitle2" 
-                                textAlign='start'
-                                gutterBottom>
-                                Running Time
-                            </Typography>
-                            <Schedule 
-                                sx={{
-                                    color : theme ? brown[300] : brown[700],
-                                    fontSize : '50px'
-                            }}/>
-                            <Typography fontWeight='bold'>
-                                {path.runtime} Min
-                            </Typography>
-                        </Box>
-                        <ClickAwayListener onClickAway={boxClickAway}>
-                            <Box 
-                                width="20%" 
-                                bgcolor={innerBoxStyle}
-                                borderRadius={4}
-                                onClick={boxClick}
-                                sx={{ p : 1,}}>
-                                <Typography 
-                                    variant="subtitle2" 
-                                    textAlign='start'
-                                    gutterBottom>
-                                    Genre
-                                </Typography>
-                                <Tag 
-                                    sx={{
-                                        color : theme ? indigo[300] : indigo[700],
-                                        fontSize : '50px'
-                                }}/>
-                                <Typography fontWeight='bold'>
-                                    {path.genres.length} Genres
-                                </Typography>
-                            </Box>
-                        </ClickAwayListener>
-                        <MoviePopper anchorEl={anchorEl} name="Genres" extra={path.genres} />
-                        <ClickAwayListener onClickAway={boxClickAway}>
-                            <Box 
-                                width="20%" 
-                                bgcolor={innerBoxStyle}
-                                borderRadius={4}
-                                onClick={boxClick}
-                                sx={{ p : 1}}>
-                                <Typography
-                                    variant="subtitle2"
-                                    textAlign='start'
-                                    gutterBottom>
-                                    Spoken Language
-                                </Typography>
-                                <Language 
-                                    sx={{
-                                        color : theme ? lightBlue[300] : lightBlue[700],
-                                        fontSize : '50px'
-                                    }} 
-                                />
-                                <Typography fontWeight='bold'>
-                                    {path.spoken_languages.length} Languages
-                                </Typography>
-                            </Box>
-                        </ClickAwayListener>
-                        <MoviePopper anchorEl={anchorEl} name="Languages" extra={path.spoken_languages}/>
+
                     </Box>
                 </Box>
             ) : (
