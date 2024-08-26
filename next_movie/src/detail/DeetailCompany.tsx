753f675e338850f57e325a4ca063896d1b833cbf
BYSooooo <avera0128@gmail.com>
2024-08-26 23:01:50 +0900

    [240826]In Dev Collection Visual

--- next_movie/src/detail/DeetailCompany.tsx
+++ next_movie/src/detail/DeetailCompany.tsx
@@ -1,10 +1,10 @@
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, Button, Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
 
 export default function DetailCompany({theme,path} : {theme : boolean, path : MovieDetail}) {
     
     return (
-        <Box display='flex' flexDirection='row' justifyContent={'space-between'} sx={{ px : 2}}>
+        <Box display='flex' flexDirection='row' justifyContent={'space-between'} sx={{ px : 2, mb : 10}}>
             <Box width="45%" flexDirection='column' >
                 <Box display='flex' flexDirection='column' alignItems='start' >
                     <Typography variant="h6" fontWeight='bold' sx={{ mt : 1, px :2}}>
@@ -30,22 +30,46 @@ export default function DetailCompany({theme,path} : {theme : boolean, path : Mo
                     { path ? (
                         <Box
                             mt={1}
-                            display="flex"
-                            borderRadius={4}
-                            sx={{
-                                backgroundImage : `url(https://image.tmdb.org/t/p/original${path.belongs_to_collection.backdrop_path})`,
-                                backgroundSize : "100%",
-                                backgroundRepeat : 'no-repeat',
-                                opacity : 0.2  
-                            }}
+                            display='flex'
                             justifyContent="center"
                             width = "100%"
-                            height = "15rem"
-                            bgcolor={theme ? grey[800] : grey[200]}>
-                            <Avatar
-                                sx={{ position : 'absolute' }}
-                                src={`https://image.tmdb.org/t/p/w500${path.belongs_to_collection.poster_path}`}
+                            height = "15rem">
+                            <Box
+                                borderRadius={4}
+                                width = "100%"
+                                overflow='hidden'
+                                zIndex={-1}
+                                sx={{
+                                    backgroundImage : `url(https://image.tmdb.org/t/p/original${path.belongs_to_collection.backdrop_path})`,
+                                    backgroundSize : "100%",
+                                    backgroundRepeat : 'no-repeat',
+                                    opacity : 0.5,
+                                }} 
                             />
+                            <Box
+                                justifyContent="center"
+                                alignItems="center"
+                                sx={{ position : 'absolute', mt : 5, zIndex : 1, display : "flex", flexDirection: "column"}}>
+                                <Avatar
+                                    sx={{
+                                        width : "5rem", 
+                                        height : "5rem",
+                                        opacity : 1,
+                                    }}
+                                    src={`https://image.tmdb.org/t/p/w500${path.belongs_to_collection.poster_path}`}
+                                />
+                                <Typography variant="h5" noWrap fontWeight="Bold">
+                                    {path.belongs_to_collection.name}
+                                </Typography>
+                                <Button 
+                                    sx={{ 
+                                        borderRadius : 2, 
+                                        bgcolor : theme ? grey[800] : grey[300],
+                                        color : theme ? "white" : "black"}}>
+                                    Detail
+                                </Button>
+                            </Box>
+
                             
                         </Box>
 
