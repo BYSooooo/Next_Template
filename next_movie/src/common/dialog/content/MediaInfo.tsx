cee6d2addf0a9b6c40f3420ecc4c414a1a2ec252
BYSooooo <avera0128@gmail.com>
2024-09-09 23:36:57 +0900

    [240909]In Dev Media Dialog in Movie Detail

--- next_movie/src/common/dialog/content/MediaInfo.tsx
+++ next_movie/src/common/dialog/content/MediaInfo.tsx
@@ -1,12 +1,16 @@
 "use client"
 
-import { Box, Button, DialogActions, DialogContent, DialogTitle, Tab, Tabs, Typography } from "@mui/material"
-import { useAppDispatch } from "../../../redux/hooks"
+import { Box, Button, DialogActions, DialogContent, DialogTitle, List, ListItemButton, ListItemIcon, ListItemText, Tab, Tabs, Typography } from "@mui/material"
+import { useAppDispatch, useAppSelector } from "../../../redux/hooks"
 import { controlDialog } from "../../../redux/features";
 import React from "react";
+import { SmartDisplay } from "@mui/icons-material";
+import ReactPlayer from "react-player";
 
 export default function MediaInfo({theme} : {theme: boolean }) {
     const [index, setIndex] = React.useState(0)
+    const [selList, setSelList] = React.useState<{idx : number, videoPath : string}>()
+    const dialogReducer = useAppSelector((state)=> state.dialogReducer).extraInfo;
     const dispatch = useAppDispatch();
     
     const onClickClose = ()=> {
@@ -43,25 +47,50 @@ export default function MediaInfo({theme} : {theme: boolean }) {
                 Media
             </DialogTitle>
             <DialogContent>
-                <Tabs value={index} onChange={handleChange}>
-                    <Tab style={tabStyle} label="Video" value={0} />
-                    <Tab style={tabStyle} label="Image" value={1} />
-                </Tabs>
-                <Box>
-                    <TabPanel value={index} index={0}>
-                        <Box width="100%" height="50vh">
+                <Box display="flex" flexDirection="row">
+                    <Box flexDirection="column">
+                        <Tabs value={index} onChange={handleChange}>
+                            <Tab style={tabStyle} label="Video" value={0} />
+                            <Tab style={tabStyle} label="Image" value={1} />
+                        </Tabs>
+                        <TabPanel value={index} index={0}>
+                            <Box width="100%" display="flex" flexDirection="row">
+                                {selList ?(
+                                    <ReactPlayer 
+                                        width="auto" height="50vh"
+                                        url={`https://www.youtube.com/watch?v=${selList.videoPath}`}
+                                    />
+                                ):(
+                                    <Box width="20rem" height="50vh">
+                                        Select List
+                                    </Box>    
+                                ) }
+                                
+                                <List sx={{ width : "10rem", height : "50vh", overflowY : "scroll"}}>
+                                    {dialogReducer.videos.results.map((video :VideoInfo, idx: number)=> {
+                                        return (
+                                            <ListItemButton 
+                                                key={video.id} 
+                                                selected={selList ? selList.idx === idx : false}
+                                                onClick={()=> setSelList({idx : idx, videoPath : video.key})} 
+                                                sx={{ borderRadius : 4}}>
+                                                <ListItemIcon>
+                                                    <SmartDisplay />
+                                                </ListItemIcon>
+                                                <ListItemText
+                                                    primary={video.name} 
+                                                    secondary={video.type} 
+                                                    sx={{ textOverflow : 'clip', textWrap : "nowrap"}}/>
+                                            </ListItemButton>
+                                        )
+                                    })}
+                                </List>
+                            </Box>
+                        </TabPanel>
+                        <TabPanel value={index} index={1}>
+                            panel2
+                        </TabPanel>
 
-                        </Box>
-                    </TabPanel>
-                    <TabPanel value={index} index={1}>
-                        panel2
-                    </TabPanel>
-                </Box>
-                <Typography variant="subtitle1">
-                    List
-                </Typography>
-                <Box width="100%" display="flex" flexDirection="row">
-                    <Box width="100%" overflow="scroll" display="flex">
                         
                     </Box>
                 </Box>
