fc05c4d29d860751fb542d7949e77e95e3e9426a
BYSooooo <avera0128@gmail.com>
2024-09-23 23:54:44 +0900

    [240923]In Dev Company Info Dialog

--- next_movie/src/common/dialog/content/CompanyInfo.tsx
+++ next_movie/src/common/dialog/content/CompanyInfo.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks"
 import { useRouter } from 'next/navigation';
-import { Button, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { Avatar, Box, Button, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 import { getCompany } from '../../../components/fetchData';
+import { controlDialog } from '../../../redux/features';
 
 export default function CompanyInfo({theme} : {theme : boolean}) {
-    const dialogReducer = useAppSelector((state)=> state.dialogReducer.extraInfo)
+    const dialogReducer : CompanyDetail = useAppSelector((state)=> state.dialogReducer.extraInfo)
     const dispatch = useAppDispatch();
     const router = useRouter();
+
+    const onClickClose = ()=> {
+        dispatch(controlDialog({ openYn : false, name : ""}));
+    }
 
     return (
         <>
@@ -15,10 +20,26 @@ export default function CompanyInfo({theme} : {theme : boolean}) {
                 Company
             </DialogTitle>
             <DialogContent>
+                <Box display="flex" flexDirection="column" width="100%">
+                    <Box flexDirection="row">
+                        {dialogReducer.logo_path ? (
+                            <Avatar
+                                sx={{ 
+                                    backgroundColor : theme ? "white" : "none2", 
+                                    mb : 1 ,
+                                    width : "8rem", height : "8rem"}}
+                                src={`https://image.tmdb.org/t/p/original${dialogReducer.logo_path}`}>
+                            </Avatar>
 
+                        ) : (
+                            <>
+                            </>
+                        )}
+                    </Box>
+                </Box>
             </DialogContent>
             <DialogActions>
-                <Button>
+                <Button onClick={onClickClose}>
                     Close
                 </Button>
             </DialogActions>
