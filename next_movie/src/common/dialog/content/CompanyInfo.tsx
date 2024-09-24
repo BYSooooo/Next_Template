8f3f20b683a37f4e0c9806ed67cb1c05a5d668a2
BYSooooo <avera0128@gmail.com>
2024-09-24 23:35:42 +0900

    [240924]In Dev Company Dialog in Movie Detail

--- next_movie/src/common/dialog/content/CompanyInfo.tsx
+++ next_movie/src/common/dialog/content/CompanyInfo.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks"
 import { useRouter } from 'next/navigation';
-import { Avatar, Box, Button, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { Avatar, Box, Button, Chip, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material';
 import { getCompany } from '../../../components/fetchData';
 import { controlDialog } from '../../../redux/features';
+import { grey } from '@mui/material/colors';
+import { Business, Public } from '@mui/icons-material';
 
 export default function CompanyInfo({theme} : {theme : boolean}) {
     const dialogReducer : CompanyDetail = useAppSelector((state)=> state.dialogReducer.extraInfo)
@@ -21,20 +23,45 @@ export default function CompanyInfo({theme} : {theme : boolean}) {
             </DialogTitle>
             <DialogContent>
                 <Box display="flex" flexDirection="column" width="100%">
-                    <Box flexDirection="row">
-                        {dialogReducer.logo_path ? (
-                            <Avatar
-                                sx={{ 
-                                    backgroundColor : theme ? "white" : "none2", 
-                                    mb : 1 ,
-                                    width : "8rem", height : "8rem"}}
-                                src={`https://image.tmdb.org/t/p/original${dialogReducer.logo_path}`}>
-                            </Avatar>
-
-                        ) : (
-                            <>
-                            </>
-                        )}
+                    <Box display="flex" flexDirection="row">
+                        <Box 
+                            display="flex"
+                            overflow="hidden" 
+                            minWidth="8rem" 
+                            height="10rem" 
+                            borderRadius={4}
+                            alignItems='center'
+                            justifyContent="center"
+                            bgcolor={theme ? grey[700] : grey[200]} 
+                            mr={2}>
+                            {dialogReducer.logo_path ? (
+                                <Box>
+                                    <img
+                                        style={{borderRadius : 5, maxWidth : '8rem', backgroundColor : "white"}} 
+                                        src={`https://image.tmdb.org/t/p/original${dialogReducer.logo_path}`}
+                                    />
+                                </Box>
+                            ) : (
+                                <Box display="flex" flexDirection="column">
+                                    <Business sx={{ width : "4rem", height : "4rem"}}/>
+                                    <Typography>
+                                        No Image
+                                    </Typography>
+                                </Box>
+                            )}
+                        </Box>  
+                        <Box display="flex" flexDirection="column">
+                            <Typography variant='h5' fontWeight='bold'>
+                                {dialogReducer.name}
+                            </Typography>
+                            <Chip
+                                component="span"
+                                sx={{ width : 'fit-content', borderRadius : 3}} 
+                                icon={<Public />} 
+                                label={dialogReducer.headquarters || "No Headquater Information"}
+                            />
+                            
+                        </Box>
                     </Box>
                 </Box>
             </DialogContent>
