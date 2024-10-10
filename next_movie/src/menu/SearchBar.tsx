676292f327ddf5f1d6b01a7ae1ed59f8d780f6c4
BYSooooo <avera0128@gmail.com>
2024-10-10 23:59:08 +0900

    [241010]In Dev Search Movie

--- next_movie/src/menu/SearchBar.tsx
+++ next_movie/src/menu/SearchBar.tsx
@@ -2,26 +2,30 @@
 
 import React, { ChangeEvent } from 'react';
 import { Search } from "@mui/icons-material";
-import { ClickAwayListener, IconButton, Input, InputAdornment, TextField } from "@mui/material";
+import { IconButton, Input, InputAdornment, TextField } from "@mui/material";
 import { useRouter } from 'next/navigation';
-import MoviePopper from '../common/popper/MoviePopper';
-
 
 export default function SearchBar() {
     const [inputValue, setInputValue] = React.useState("");
-    const [anchorEl, setAnchorEl] = React.useState<HTMLElement|null>(null)
+    const [inputStatus, setInputStatus] = React.useState(false)
     const router = useRouter();
-    const clickAwayHandler = ()=> setAnchorEl(null)
+    
+    React.useEffect(()=> {
+        setInputStatus(false);
+    },[])
 
     const onClickSearch = (event : React.MouseEvent<HTMLElement>)=> {
-        inputValue.trim().length > 0
-        ? router.push(`/search/${inputValue.trim()}`)
-        : setAnchorEl(event.currentTarget)
-        
+        if(inputValue.trim().length > 0) {
+            router.push(`/search/${inputValue.trim()}`)
+            setInputStatus(false)
+        } else {
+            setInputStatus(true)
+        }
     }
 
     const onChangeSearchInput = (event : ChangeEvent<HTMLInputElement>)=> {
         setInputValue(event.target.value)
+        setInputStatus(false)
     }
 
     return (
@@ -40,6 +44,7 @@ export default function SearchBar() {
             placeholder="Search"
             color="primary"
             size="small"
+            error={inputStatus}
             endAdornment={
               <InputAdornment position="end">
                     <IconButton 
