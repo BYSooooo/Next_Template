71246ffd97c9fd21d26f09d35b184e9f763727d6
BYSooooo <92851138+BYSooooo@users.noreply.github.com>
2024-12-13 23:02:37 +0900

    Update FriendList.tsx

--- next_messenger/src/main/FriendList.tsx
+++ next_messenger/src/main/FriendList.tsx
@@ -1,10 +1,10 @@
 export default function FriendList() {
 
     return (
-        <>
+        <div className='max-w-[15rem] w-[30vw] bg-cyan-200'>
             <p className="text-red-500">
                 This is Friend List
             </p>
-        </>
+        <div/>
     )
 }
