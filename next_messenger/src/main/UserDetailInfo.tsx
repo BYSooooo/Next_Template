a025e209f9b9ba86c3cda7b4af59878b8ac1553a
BYSooooo <avera0128@gmail.com>
2025-01-15 23:36:44 +0900

    [250115]In Dev Profile View

--- next_messenger/src/main/UserDetailInfo.tsx
+++ next_messenger/src/main/UserDetailInfo.tsx
@@ -1,8 +1,34 @@
+"use client"
+
+import { useAppSelector } from "../redux/hooks"
+
 export default function UserDetailInfo() {
+    const userInfoSlice = useAppSelector((state)=> state.userStore);
 
     return (
-        <div>
-            This is UserInfo
+        <div className='default-box
+            flex flex-col max-w-[79vw] ml-1 w-[100vw] p-5' >
+            <p className="font-bold text-start text-5xl">
+                Profile
+            </p>
+            <div className="flex flex-row gap-3">
+                {/* DisplayName*/}
+                <div className="">
+                    <p >
+                        DisplayName
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        Profile Photo
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        Email Change
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
