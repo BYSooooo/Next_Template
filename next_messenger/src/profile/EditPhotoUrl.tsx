f23b78d8ebac4ffe02d4703742f7dec5ada0733e
BYSooooo <avera0128@gmail.com>
2025-01-29 23:56:39 +0900

    [250129]In Dev Profile Edit Page in Messenger

--- next_messenger/src/profile/EditPhotoUrl.tsx
+++ next_messenger/src/profile/EditPhotoUrl.tsx
@@ -1,14 +1,27 @@
+import { UserCircleIcon } from "@heroicons/react/24/solid"
+
 export default function EditPohtoUrl({photoUrl} : {photoUrl : string}) {
+    const photoCircle = ()=> {
+
+    }
+
     return (
+        
         <div>
+            <p>
+                Photo
+            </p>
             {photoUrl 
-                ? <p>
-                    Yes
-                </p>
-                : <p>
-                    No
-                </p>
+                ? 
+                    <p>
+                        Yes
+                    </p>
+                : 
+                    <UserCircleIcon className="w-32 h-32"/> 
             }
+            <button>
+                Edit
+            </button>
         </div>
     )
 }
