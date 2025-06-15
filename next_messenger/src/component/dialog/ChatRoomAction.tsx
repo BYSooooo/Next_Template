ad5b65e768089429064eb565f48988d8679ff891
BYSooooo <avera0128@gmail.com>
2025-06-16 00:10:33 +0900

    [250616]Developing of ChatRoom Message

--- next_messenger/src/component/dialog/ChatRoomAction.tsx
+++ next_messenger/src/component/dialog/ChatRoomAction.tsx
@@ -1,11 +1,33 @@
 "use client";
+import { PhotoIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 
 export default function ChatRoomAction() {
 
+    const hoverStyle ="p-[0.2rem] hover:bg-slate-300 hover:dark:bg-slate-700 rounded-md dark:hover:bg-slate-500"
+    
+    const onClickHandler = (name: string)=> {
+
+    };
+
     return (
-        <div className="flex flex-col items-center">
-            
+        <div className="flex flex-row items-center">
+            <div className='h-[15%] flex flex-col gap-2'>
+                <button
+                    className={`${hoverStyle}`}
+                    // onClick={()=> onClickHandler()} 
+                    >
+                    <span className='flex flex-row items-center gap-2'>
+                        <PhotoIcon className='w-7 h-7'/>
+                        <p>Photo List</p>
+                    </span>
+                </button>
+
+                
+            </div>
+            <div className='h-[85%]'>
+                Hello
+            </div>
         </div>
     )
 }
