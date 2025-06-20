77222d43183eb24335a2ff91074d334500797437
BYSooooo <avera0128@gmail.com>
2025-06-20 23:53:04 +0900

    [250620]Developing of Chat Room Extra Action

--- next_messenger/src/component/dialog/ChatRoomArchive.tsx
+++ next_messenger/src/component/dialog/ChatRoomArchive.tsx
@@ -1,22 +1,12 @@
 "use client";
 
 import React from 'react';
-import { getChatRoomFile } from '../../controller/FirebaseController';
-
 
 export default function ChatRoomArchive({chatId, uuid} : {chatId : string, uuid : string}) {
-    const [ fileStrings, setFileStrings ] = React.useState(); 
-
+    
     React.useEffect(()=> {
         
     },[])
-
-    const fileList = async() => {
-        const { result, value } = await getChatRoomFile(chatId, uuid);
-        if(result) {
-            
-        }
-    }
 
     return (
         <div>
--- next_messenger/src/component/dialog/ChatRoomPhoto.tsx
+++ next_messenger/src/component/dialog/ChatRoomPhoto.tsx
@@ -1,4 +1,13 @@
+"use client";
+
+import React from 'react';
+
 export default function ChatRoomPhoto() {
+    const [ fileStrings, setFileStrings ] = React.useState(); 
+
+    React.useEffect(()=> {
+        
+    },[]);
 
     return (
         <div>
