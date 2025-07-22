287e8c6b798376f5bea188657d9b646215015f74
BYSooooo <avera0128@gmail.com>
2025-07-22 23:57:40 +0900

    [250722]Develop of Chat Message Export

--- next_messenger/src/component/dialog/ChatRoomArchive.tsx
+++ next_messenger/src/component/dialog/ChatRoomArchive.tsx
@@ -8,9 +8,37 @@ export default function ChatRoomArchive() {
         
     },[])
 
+    const onClickExportText = ()=> {
+
+    }
+
+    const onClickExportCSV = ()=> {
+
+    }
+
     return (
-        <div>
-            This is Chat Room Archive
+        <div className='flex flex-col '>
+            <h1 className='font-bold text-sm mb-1'>
+                Messages Export
+            </h1>
+            <ul>
+                <li className='text-xs'>
+                    You can export Messages.
+                </li>
+                <li>
+                    Attachments Files are not export.
+                </li>
+            </ul>
+            <button 
+                className='default-button'
+                onClick={onClickExportText}>
+                Export TXT
+            </button>
+            <button 
+                className='default-button'
+                onClick={onClickExportCSV}>
+                Export CSV
+            </button>
         </div>
     )
 }
