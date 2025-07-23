94118ce37596d13cebe10c6b4fec4a9a1f183bc0
BYSooooo <avera0128@gmail.com>
2025-07-23 22:54:03 +0900

    [250723]Develop of Chat Message Export

--- next_messenger/src/component/dialog/ChatRoomArchive.tsx
+++ next_messenger/src/component/dialog/ChatRoomArchive.tsx
@@ -1,15 +1,60 @@
 "use client";
 
 import React from 'react';
+import { useAppSelector } from '../../redux/hooks';
+import ChatItem from '../../chat/ChatItem';
+import { getSelectedUserInfo } from '../../controller/FirebaseController';
 
 export default function ChatRoomArchive() {
-    
+    const chatStore = useAppSelector((state)=> state.chatStore);
     React.useEffect(()=> {
         
     },[])
 
-    const onClickExportText = ()=> {
+    /*
+    const onClickExportText = (e : React.MouseEvent)=> {
+        e.preventDefault();
+        if(chatStore.messages.length > 0) {
+            const array = []
+            
+            for (const item of chatStore.messages) {
+                
+                const response = await getSelectedUserInfo({uuid : item.createdBy, chatId : chatStore})
+                const context = {
+                    time : item.createdAt.toLocaleString(),
+                    sender : 
 
+                }
+            }
+            chatStore.forEach((chatItem)=> {
+
+                if(chatItem.content.length > 0) {
+                    const context = {
+                        time : chatItem.createdAt.toLocaleString(),
+                        sender : chatItem.createdBy,
+                        message : chatItem.content,
+                    }
+                    array.push(context);
+                }
+            })
+            messageDown(JSON.stringify(array, null, "\t"), `export_msg_${new Date().toLocaleDateString()}.txt`, 'text/txt');
+        }
+
+    }
+    */
+
+    const messageDown = (data: BlobPart, fileName : string, fileType : string) => {
+        const blob = new Blob([data], { type : fileType});
+        const a = document.createElement('a');
+        a.download = fileName;
+        a.href = window.URL.createObjectURL(blob);
+        const clickEvent = new MouseEvent('click', {
+            view : window,
+            bubbles : true,
+            cancelable : true
+        });
+        a.dispatchEvent(clickEvent);
+        a.remove()
     }
 
     const onClickExportCSV = ()=> {
@@ -31,7 +76,8 @@ export default function ChatRoomArchive() {
             </ul>
             <button 
                 className='default-button'
-                onClick={onClickExportText}>
+                //onClick={onClickExportText}
+                >
                 Export TXT
             </button>
             <button 
