b46b7373a2f956accf79c0497803cdf8203914f8
BYSooooo <avera0128@gmail.com>
2025-11-29 23:58:04 +0900

    [251129]In Dev Template

--- next_template_v2/src/app/page.tsx
+++ next_template_v2/src/app/page.tsx
@@ -1,23 +1,29 @@
 import { Title } from "@mui/icons-material";
-import { Card, CardContent, CardHeader, Container, Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Container, Grid, Typography } from "@mui/material";
 
 export default function Page() {
     return (
-        <Container>
-            <Card>
-                <Typography 
-                    fontStyle={"italic"}
-                    fontFamily={"system-ui"}>
-                        This is Card Header
-                </Typography>
-                <CardHeader>
-                    
-                </CardHeader>
-                <CardContent>
+            <Grid container sx={{ mt : 3}} spacing={2}>
 
-                </CardContent>
-            </Card>
-        </Container>
+                <Card>
+                    <Typography 
+                        fontStyle={"italic"}
+                        fontFamily={"system-ui"}>
+                            This is Card Header
+                    </Typography>
+                    <CardHeader>
+                        
+                    </CardHeader>
+                    <CardContent>
+
+                    </CardContent>
+                </Card>
+                <Card>
+                    <Typography>
+                        This is Card Header
+                    </Typography>
+                </Card>
+            </Grid>
         
     )
 }
