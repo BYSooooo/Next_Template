319fcbd4c57fa343410a6b588550e33f4ccdfd1d
BYSooooo <avera0128@gmail.com>
2025-12-04 23:59:03 +0900

    [251204]In Dev Side Bar

--- next_template_v2/src/detail/component/MessengerDetail.tsx
+++ next_template_v2/src/detail/component/MessengerDetail.tsx
@@ -1,9 +1,27 @@
+import { Box, Button, Divider, List, ListItem, ListItemText, Paper, Typography } from "@mui/material";
+
 
 export default function MessengerDetail() {
 
     return (
-        <div>
-            This is msg Detail
-        </div>
+        <Box sx={{ maxWidth : '30vw'}}>
+            <Typography variant="subtitle1" fontWeight={'bold'}>
+                Description
+            </Typography>
+            <Paper sx={{ p : 2}}>
+                <Typography>
+                    Search for users and add them to your friends list through a request and acceptance process.
+                </Typography>
+                <Divider />
+                <Typography>
+
+                </Typography>
+            </Paper>
+            <Button >
+                Hello
+            </Button>
+        </Box>
+        
+        
     )
 }
