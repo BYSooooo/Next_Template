61b44a9bc3d159eae423e903039ff3a8565da2bb
BYSooooo <avera0128@gmail.com>
2025-12-12 23:53:22 +0900

    [251212]In Dev Preview Image Slider

--- next_template_v2/src/detail/component/MessengerDetail.tsx
+++ next_template_v2/src/detail/component/MessengerDetail.tsx
@@ -1,18 +1,19 @@
 import { AttachFile, Chat, Login, Person } from "@mui/icons-material";
-import { Box, List, Paper, Typography } from "@mui/material";
+import { Box, List, Paper, Stack, Typography } from "@mui/material";
 import DetailListItem from "./DetailListItem";
-import main from '../../../public/asset/main.png';
 
-/* Swiper */
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules'
+/* Static Image */
+import image01 from '../../../public/asset/movie01.png';
+import image02 from '../../../public/asset/movie02.png';
+import image03 from '../../../public/asset/movie03.png';
+import image04 from '../../../public/asset/movie04.png';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
 import Image from "next/image";
 
 
 export default function MessengerDetail() {
+
+    
 
     return (
         <Box>
@@ -37,15 +38,22 @@ export default function MessengerDetail() {
                 Preview
             </Typography>
             <Paper sx={{ px : 2, rowGap : 1}}>
-                <Swiper navigation={true} modules={[Navigation]}>
+                {/* <Swiper 
+                    direction="vertical"
+                    navigation={true} modules={[Navigation]}>
                     <SwiperSlide>
-                        <Image 
-                            src={main}
-                            alt="Preview01"
-                            layout="responsive"
-                        />
+                        <Image src={image01} alt="Preview01" height={100}  /> 
                     </SwiperSlide>
-                </Swiper>
+                     <SwiperSlide>
+                        <Image src={image02} alt="Preview02"  height={100}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Image src={image03} alt="Preview03"  /> 
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Image src={image04} alt="Preview04"/>
+                    </SwiperSlide>
+                </Swiper> */}
             </Paper>
         </Box>
         
