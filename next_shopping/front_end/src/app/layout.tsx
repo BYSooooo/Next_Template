de5616f0071c1b9867805adc5009fb301d0932a4
BYSooooo <avera0128@gmail.com>
2026-01-21 23:59:02 +0900

    [260121]In Dev Front End

--- next_shopping/front_end/src/app/layout.tsx
+++ next_shopping/front_end/src/app/layout.tsx
@@ -1,10 +1,13 @@
-import Footer from './footer';
 import './globals.css';
+
+import Footer from './footer';
+import Header from './Header';
 
 export default function RootLayout({children} : {children : React.ReactNode}) {
     return (
         <html lang="en">
             <body className='@container'>
+                <Header />
                 {children}
                 <Footer/>
             </body>
