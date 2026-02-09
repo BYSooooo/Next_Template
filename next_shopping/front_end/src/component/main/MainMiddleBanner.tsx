acb3e647324e75aacc753b7c086344dba911afaa
BYSooooo <avera0128@gmail.com>
2026-02-09 23:58:28 +0900

    [260209]In Dev Front Layout

--- next_shopping/front_end/src/component/main/MainMiddleBanner.tsx
+++ next_shopping/front_end/src/component/main/MainMiddleBanner.tsx
@@ -1,9 +1,10 @@
 export default function MainMiddleBanner() {
     return (
         <div className="w-full">
-            <div className="mx-auto max-w-7xl px-6 py-4">
-                This is Banner
-
+            <div className="flex mx-auto max-w-7xl px-6 py-4 justify-center">
+                <div className="bg-gray-100 rounded-xl w-full h-32">
+                    This is Banner Position
+                </div>
             </div>
         </div>
     )
