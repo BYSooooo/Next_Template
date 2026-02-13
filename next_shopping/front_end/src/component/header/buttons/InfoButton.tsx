40de585110cabcd4bc91ab25af74475af5bfb4f3
BYSooooo <avera0128@gmail.com>
2026-02-13 23:27:32 +0900

    [260213]In Dev Front End

--- next_shopping/front_end/src/component/header/buttons/InfoButton.tsx
+++ next_shopping/front_end/src/component/header/buttons/InfoButton.tsx
@@ -6,31 +6,41 @@ import { Button, Modal, Popover } from '@heroui/react';
 import React from 'react';
 
 export default function InfoButton() {
+    const [popoverOpen, setPopoverOpen] = React.useState(false);
+    const [modelOpen, setModalOpen] = React.useState(false);
+
+    const onClickSignIn = ()=> {
+        setPopoverOpen(false)
+        setModalOpen(true)
+    }
     
     return (
-        <Popover>
-            <Button variant='outline' isIconOnly >
-                <UserIcon />
-            </Button>
-            <Popover.Content className="max-w-64">
-                <Popover.Arrow />
-                <Popover.Dialog>
-                    <Popover.Heading className='text-center'>
-                        User Profile    
-                    </Popover.Heading>
-                    <div className='flex flex-row gap-2 mt-2'>
-                        <Modal>
+        <>
+        
+            <Popover isOpen={popoverOpen}>
+                <Button variant='outline' isIconOnly >
+                    <UserIcon />
+                </Button>
+                <Popover.Content className="max-w-64">
+                    <Popover.Arrow />
+                    <Popover.Dialog>
+                        <Popover.Heading className='text-center'>
+                            User Profile    
+                        </Popover.Heading>
+                        <div className='flex flex-row gap-2 mt-2'>
+                            <Modal>
+                                <Button variant='outline' size='sm' fullWidth>
+                                    <p className='text-xs'>Sign In</p>
+                                    <LoginModal />
+                                </Button>
+                            </Modal>
                             <Button variant='outline' size='sm' fullWidth>
-                                <p className='text-xs'>Sign In</p>
-                                <LoginModal />
+                                <p className='text-xs'>Sign Up</p>
                             </Button>
-                        </Modal>
-                        <Button variant='outline' size='sm' fullWidth>
-                            <p className='text-xs'>Sign Up</p>
-                        </Button>
-                    </div>
-                </Popover.Dialog>
-            </Popover.Content>
-        </Popover>
+                        </div>
+                    </Popover.Dialog>
+                </Popover.Content>
+            </Popover>
+        </>
     )
 }
