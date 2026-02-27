b905565d526905f255b99695ad57eaebc9e0c4f9
BYSooooo <avera0128@gmail.com>
2026-02-27 23:59:08 +0900

    [260227]In Dev Sign In Form

--- next_shopping/front_end/src/component/login/ManualLogin.tsx
+++ next_shopping/front_end/src/component/login/ManualLogin.tsx
@@ -1,18 +1,33 @@
+'use client'
+
+import React from 'react';
 import { EnvelopeIcon } from "@heroicons/react/24/outline";
-import { Button, Input } from "@heroui/react";
+import { Button, FieldError, Input, TextField } from "@heroui/react";
 
 export default function ManualLogin() {
+    const [ signInEmail, setSignInEmail] = React.useState("");
+
     const onPressSignIn = ()=> {
 
     }
 
+    const emailValidYn = true
+
     return (
         <>
+            <TextField isInvalid={emailValidYn}>
             <Input
                 fullWidth
                 placeholder="Input Email..." 
+                value={signInEmail}
                 className='border-2 border-solid border-black focus:border-none'
                 />
+            {signInEmail.length === 0 &&
+                <FieldError>
+                    Email is Required.
+                </FieldError>
+            }
+            </TextField>
             <Button
                 onPress={onPressSignIn}
                 className="w-full flex flex-row">
