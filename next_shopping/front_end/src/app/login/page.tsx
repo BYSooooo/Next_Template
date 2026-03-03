b87f30a6c540b9e86a9bed14d8848cd2edd01c9d
BYSooooo <avera0128@gmail.com>
2026-03-03 23:57:11 +0900

    [260303]In Dev Login Page

--- next_shopping/front_end/src/app/login/page.tsx
+++ next_shopping/front_end/src/app/login/page.tsx
@@ -1,35 +1,44 @@
 "use client"
 
-import { Card, Label, Separator } from "@heroui/react";
+import { Card, Input, Label, Separator, TextField } from "@heroui/react";
 import { useState } from "react"
 
 export default function Page() {
     const [ email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [ password, setPassword] = useState("");
+    const [ validYn, setValidYn ] = useState(false);
 
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center justify-center h-[80vh]">
             <p className="font-bold text-xl">
                 Login
             </p>
-            <div className="flex flex-row">
-                <div className="flex flex-col">
-                    <Label>
+            <div className="flex flex-row border-2 border-red-500 rounded-md">
+                <Card className="bg-gray-50 w-65">
+                    <Card.Title className="font-bold shadow-accent-soft">
                         Select Service
-                    </Label>
-                    <Card>
+                    </Card.Title>
+                    <Card.Content>
 
-                    </Card>
-                </div>
+                    </Card.Content>
+                </Card>
                 <Separator orientation="vertical"/>
-                <div className="flex flex-col">
-                    <Label>
+                <Card className="bg-gray-50 w-65">
+                    <Card.Title className="font-bold shadow-accent-soft">
                         Input Email/Password
-                    </Label>
-                    <Card>
-
-                    </Card>
-                </div>
+                    </Card.Title>
+                    <Card.Content>
+                        <TextField>
+                            <Label>Email</Label>
+                            <Input
+                                fullWidth
+                                placeholder="input Email..."
+                                className="border-2 border-solid border-black focus:outline-0 focus:ring-0"
+                            />
+                            
+                        </TextField>
+                    </Card.Content>
+                </Card>
             </div>
         </div>
     )
