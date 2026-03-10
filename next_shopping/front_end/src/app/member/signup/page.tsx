81f2a981f9bfe05a325a5c13165824268792c205
BYSooooo <avera0128@gmail.com>
2026-03-10 23:56:05 +0900

    [260310]In Dev Front layout

--- next_shopping/front_end/src/app/member/signup/page.tsx
+++ next_shopping/front_end/src/app/member/signup/page.tsx
@@ -1,7 +1,35 @@
+import { Card, Separator } from "@heroui/react";
+
 export default function Page() {
     return (
-        <div>
-            
+        <div className="inner-container flex items-center h-screen justify-center flex-row">
+            <div className="grid grid-cols-12">
+
+            </div>
+            <div className="flex flex-col grid-cols-5">
+                <p className="text-5xl">
+                    Wellcome to 
+                </p>
+                <div className="flex flex-row">
+                    <p className="text-5xl font-extrabold text-yellow-400">
+                        Next
+                    </p>
+                    <p className="text-5xl font-extrabold">
+                        Shopping!
+                    </p>
+                </div>
+
+            </div>
+            <Separator orientation="vertical" className="grid-cols-2"/>
+            <Card className="grid-cols-5">
+                <Card.Header>
+                    Information
+                </Card.Header>
+                <Card.Content>
+                    
+                </Card.Content>
+            </Card>
+
         </div>
     )
 }
