d76ae8577bcfb47270f36e61d77fd4fff6574478
BYSooooo <avera0128@gmail.com>
2026-03-14 23:57:00 +0900

    [260314]In Dev Front End Sing In

--- next_shopping/front_end/src/app/member/signup/page.tsx
+++ next_shopping/front_end/src/app/member/signup/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from 'react';
 import { Button, Card, Form, Input, Label, Separator, TextField } from "@heroui/react";
+import { confirmOTP, sendVerificationCode } from '@/lib/supabase/authAction';
 
 export default function Page() {
 
@@ -10,6 +11,8 @@ export default function Page() {
     const [verifyCode, setVerifyCode] = React.useState("");
     const [timer, setTimer] = React.useState(300) // 5 Minute
     const [loading, setLoading] = React.useState(false)
+
+    
 
     // Timer 
     useEffect(()=> {
@@ -26,11 +29,34 @@ export default function Page() {
 
     // Change Time Format such as '05:00'
     const formatTime = React.useCallback((seconds : number)=> {
-        
-    })
+        const mins = Math.floor(seconds / 60);
+        const secs = seconds % 60;
+        return `${String(mins).padStart(2,'0')}:${String(secs).padStart(2,'0')}`
+    },[])
 
-    const onPressVerifyCode = ()=> {
-        
+    const onPressVerifyCode = async()=> {
+        setLoading(true);
+        try {
+            await sendVerificationCode(email)
+            
+            setSendYn(true);
+            setTimer(300);
+            setVerifyCode("");
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setLoading(false);
+        }
+    }
+    
+    const onPressConfimVerifyCode = async()=> {
+        setLoading(true);
+        try {
+            await confirmOTP(email, verifyCode);
+            
+        } catch(error) {
+
+        }
     }
 
     return (
@@ -79,6 +105,13 @@ export default function Page() {
                                     placeholder='Input Verify Code...'
                                     className="border-2 border-solid border-black focus:outline-0 focus:ring-0"
                                 />
+                                <Button 
+                                    fullWidth
+                                    isDisabled={sendYn}
+                                    variant={sendYn ? 'ghost' : 'secondary' }
+                                    onPress={onPressConfimVerifyCode}>
+                                    Verify
+                                </Button>
                             </TextField>
                         </Card.Content>
                     </Card>
