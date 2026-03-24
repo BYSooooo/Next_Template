0c80742b7ee37655ff3f6b757cd7cf1d2cab8874
BYSooooo <avera0128@gmail.com>
2026-03-24 23:43:08 +0900

    [260324]In Dev Front End Sign Up Layout

--- next_shopping/front_end/src/app/member/signup/page.tsx
+++ next_shopping/front_end/src/app/member/signup/page.tsx
@@ -7,45 +7,35 @@ import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/24/outline';
 
 export default function Page() {
     const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
-    const [popOpen, setPopOpen] = React.useState(false);
-
-    // Check Condition of Password
-    const [condition, setCondition] = React.useState({
-        length : false,
-        hasUpperCase : false,
-        hasLowerCase : false,
-        hasNumber : false,
-        hasSpecial : false
-    })
-
+    const [submitYn, setSubmitYn] = React.useState(false);
     const [sendYn, setSendYn] = React.useState(false);
     const [verifyCode, setVerifyCode] = React.useState("");
     const [timer, setTimer] = React.useState(300) // 5 Minute
     const [loading, setLoading] = React.useState(false)
 
-    const onChangePassword = (value: string)=> {
-        setPassword(value);
+    // Test for Email Format Validation
+    const isEmailValid = React.useMemo(()=> {
+        const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return pattern.test(email);
+    },[email])
 
-        setCondition({
-            length : value.length >= 12 ,
-            hasUpperCase : /[A-Z]/.test(value),
-            hasLowerCase : /[a-z]/.test(value),
-            hasNumber : /[0-9]/.test(value),
-            hasSpecial : /[!@#$%^&*]/.test(value)
-        })
-    };
+    // Error Message for Email Input
+    const emailErrorMessage = React.useMemo(()=> {
+        if(!submitYn) return "";
+        if(email.length === 0) return "Please Input Email";
+        if(!isEmailValid) return "Invalid Email Address";
+        return ""
+    },[submitYn, email, isEmailValid])
+
 
     // Timer 
     useEffect(()=> {
         let interval: number;
-
         if(sendYn && timer > 0) {
             interval = window.setInterval(()=> {
                 setTimer((prev)=>  prev -1)
             }, 1000)
         }
-
         return () => window.clearInterval(interval)
     },[sendYn, timer])
 
@@ -55,42 +45,37 @@ export default function Page() {
         return `${String(mins).padStart(2,'0')}:${String(secs).padStart(2,'0')}`
     },[])
 
-    const isEmailValid = React.useMemo(()=> {
-        const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return pattern.test(email);
-    },[email])
-
-    const isPasswordValid = React.useMemo(()=> {
-
-    },[])
-
+    // Send Verify Code 
     const onPressVerifyCode = async()=> {
-        const emailTest = //
-        
-        setLoading(true);
-        try {
-            await sendVerificationCode(email)
-            
-            setSendYn(true);
-            setTimer(300);
-            setVerifyCode("");
-        } catch (error) {
-            console.error(error)
-        } finally {
-            setLoading(false);
+        setSubmitYn(true)
+        if(!isEmailValid) {
+            return;
+        } else {
+            setLoading(true);
+            try {
+                await sendVerificationCode(email)
+                setSendYn(true);
+                setTimer(300);
+                setVerifyCode("");
+            } catch (error) {
+                console.error(error)
+            } finally {
+                setLoading(false);
+            }
         }
     }
-  
+    
+    // Confirm For Email send Verified Code
     const onPressConfimVerifyCode = async()=> {
         setLoading(true);
         try {
             const { data, error } = await confirmOTP(email, verifyCode);
             if(!error) {
-
+                //....
             } else {
                 throw new Error(error.message);
             }
-            // data.user.em
+            
         } catch(error) {
             console.log(error)
         } finally{
@@ -126,72 +111,6 @@ export default function Page() {
                                     placeholder='Input Email...'
                                     className="border-2 border-solid border-black focus:outline-0 focus:ring-0"
                                 />
-                                <Label>Password</Label>
-                                <div className='relative'>
-                                    <Input
-                                        fullWidth
-                                        onChange={(e)=>onChangePassword(e.target.value)}
-                                        value={password}
-                                        onFocus={()=> setPopOpen(true)}
-                                        onBlur={(e)=> {
-                                            if(e.relatedTarget?.closest('[data-popover]')) return;
-                                            setPopOpen(false)
-                                        }}
-                                        placeholder='Input Password...'
-                                        type='password'
-                                        className="border-2 border-solid border-black focus:outline-0 focus:ring-0"
-                                    />
-
-                                    <Popover isOpen={popOpen}>
-                                        <Popover.Content>
-                                            <div data-popover tabIndex={-1}>
-                                                <Popover.Dialog>
-                                                    <Popover.Heading className='flex flex-col gap-2'>
-                                                        <p className='font-semibold'>
-                                                            The Password must meet the following condition.
-                                                        </p>
-                                                        <div className='flex flex-row gap-2'>
-                                                            { condition.length 
-                                                                ? <CheckCircleIcon className='text-green-500 w-5 h-5'/> 
-                                                                : <XCircleIcon className='text-red-500 w-5 h-5'/> 
-                                                            }
-                                                            <p>
-                                                                12 characters or more
-                                                            </p>
-                                                        </div>
-                                                        <div className='flex flex-row gap-2'>
-                                                            { condition.hasLowerCase 
-                                                                ? <CheckCircleIcon className='text-green-500 w-5 h-5'/> 
-                                                                : <XCircleIcon className='text-red-500 w-5 h-5'/>
-                                                            }
-                                                            <p>
-                                                                Contain at least one English upper case     
-                                                            </p>
-                                                        </div>
-                                                        <div className='flex flex-row gap-2'>
-                                                            {condition.hasLowerCase
-                                                                ? <CheckCircleIcon className='text-green-500 w-5 h-5'/> 
-                                                                : <XCircleIcon className='text-red-500 w-5 h-5'/>    
-                                                            }
-                                                            <p>
-                                                                Contain at least one English lower case
-                                                            </p>
-                                                        </div>
-                                                        <div className='flex flex-row gap-2'>
-                                                            { condition.hasSpecial
-                                                                ? <CheckCircleIcon className='text-green-500 w-5 h-5'/> 
-                                                                : <XCircleIcon className='text-red-500 w-5 h-5'/>    
-                                                            }
-                                                            <p>
-                                                                One or more special character 
-                                                            </p>
-                                                        </div>
-                                                    </Popover.Heading>
-                                                </Popover.Dialog>
-                                            </div>
-                                        </Popover.Content>
-                                    </Popover>
-                                </div>
                                 <Button 
                                     onPress={onPressVerifyCode}
                                     className="w-full">
