4c1128bb78de8860dc71afedec22fdfdcb953927
BYSooooo <avera0128@gmail.com>
2026-04-16 23:56:31 +0900

    [260416]In Dev Front End  Layout

--- next_shopping/front_end/src/app/member/userinfo/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-
-    return (
-        <div className="inner-container flex items-center h-screen justify-center">
-            This is User Info
-        </div>
-    )
-}
