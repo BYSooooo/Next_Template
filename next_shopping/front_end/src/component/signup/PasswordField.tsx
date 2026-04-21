36d3e2270c87d57221e9163386afa71fae71d4d2
BYSooooo <avera0128@gmail.com>
2026-04-21 23:55:26 +0900

    [260421]In Dev Front End Sign Up Layout

--- next_shopping/front_end/src/component/signup/PasswordField.tsx
+++ next_shopping/front_end/src/component/signup/PasswordField.tsx
@@ -1,15 +1,39 @@
-import { Input, Label, TextField } from "@heroui/react";
+"use client";
+
+import React from 'react';
+
+import { Description, Input, Label, TextField } from "@heroui/react";
 
 export default function PasswordField() {
+    const [inputPwd, setInputPwd] = React.useState("");
+    const [inputConfirm, setInputConfirm] = React.useState("");
+
+    const isRegexPwd = React.useMemo(()=> {
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/;
+        return regex.test(inputPwd);
+    },[inputPwd])
+
+    const isMatched = inputPwd.length > 0 && inputPwd === inputConfirm;
     
 
     return (
         <div>
-            <TextField>
+            <TextField
+                type="password"
+                onChange={setInputPwd}
+                isInvalid={inputPwd.length > 0 && !isRegexPwd}>
                 <Label isRequired>Password</Label>
-                <Input className="form-input"/>
+                <Input 
+                    className="form-input"/>
+                <Description className='text-xs text-red-500'>
+                    { isRegexPwd 
+                        ? 'Meet password requirement'
+                        : 'require : 12 characters, upper/lowercase, special character'
+                    }
+                </Description>
             </TextField>
-            <TextField>
+            <TextField
+                onChange={setInputConfirm}>
                 <Label isRequired>Password Confirm</Label>
                 <Input className="form-input"/>
             </TextField>
