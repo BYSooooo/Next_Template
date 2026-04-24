3e5968a5503413290fda66312655aa0603dd2c9f
BYSooooo <avera0128@gmail.com>
2026-04-24 23:19:58 +0900

    [260424]In Dev Front End Layout

--- next_shopping/front_end/src/component/signup/NicknameField.tsx
+++ next_shopping/front_end/src/component/signup/NicknameField.tsx
@@ -5,29 +5,63 @@ import { Button, Description, Input, Label, TextField } from "@heroui/react";
 
 export default function NicknameField() {
     const [inputNick, setInputNick] = React.useState("");
+    // null = not yet check, boolean = check result
+    const [isUsed, setisUsed] = React.useState<boolean|null>(null);
+    const [isTouched, setIsTouched] = React.useState(false);
 
     const onPressCheckNickName = ()=> {
-        //....
+        if(!isRegexPwd) return;
+
+        // TODO: Check Logic with Supabase
+        // ...
+        const isDuplicate = false
+        setisUsed(isDuplicate);
+
     }
+
+    const isRegexPwd = React.useMemo(()=> {
+        const regex = /^[a-zA-Z0-9]{5,12}$/;
+        return regex.test(inputNick)
+    },[inputNick]);
+
+    const hasError = isTouched && (!isRegexPwd || isUsed === true);
 
     return (
         <div className='flex flex-col gap-2'>
             <TextField
+                isInvalid={hasError}
                 onChange={(e)=> {
-                    setInputNick(e)
+                    setInputNick(e);
+                    setisUsed(null);
+                    if(!isTouched) setIsTouched(true)
                 }}>
-                <Label 
-                    isRequired>Nickname</Label>
-                <Input className="form-input"/>
+                <Label isRequired>
+                    Nickname
+                </Label>
+                <div className='flex flex-row gap-2'>
+                    <Input className="form-input w-[75%]"/>
+                    <Button
+                        isDisabled={!isRegexPwd}
+                        onPress={onPressCheckNickName}
+                        className="bg-yellow-400 text-black w-[20%]">
+                        Check
+                    </Button>
+                </div>
             </TextField>
+            { isTouched && 
+                // (  
+                //     ?   <Description>
+                //             This Nickname is already used.
+                //         </Description>
+                //     :    <Description>
+                //             ✓ This NickName available.
+                //         </Description>
+                     
+                // )
+            }
             <Description>
 
             </Description>
-            <Button
-                onPress={onPressCheckNickName}
-                className="w-full bg-yellow-400 text-black">
-                Check Nickname
-            </Button>
 
         </div>
     )
