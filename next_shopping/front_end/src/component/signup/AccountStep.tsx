85acc760290240384974db83997d22b263be474d
BYSooooo <avera0128@gmail.com>
2026-04-28 00:53:45 +0900

    [260428]in Dev Front End Layout Sign Up

--- next_shopping/front_end/src/component/signup/AccountStep.tsx
+++ next_shopping/front_end/src/component/signup/AccountStep.tsx
@@ -52,12 +52,14 @@ export default function AccountStep() {
         const isPasswordValid = isRegexPwd && isMatched;
         const isNicknameValid = isRegexNick && isNickUsed === false;
 
+        /*
         if(!isPasswordValid || !isNicknameValid) {
             setIsPwdTouched(true);
             setIsComfirmTouched(true);
             setIsNickTouched(true)
             return;
         } 
+        */
 
         setInfo({
             password : inputPwd,
--- next_shopping/front_end/src/component/signup/ContactStep.tsx
+++ next_shopping/front_end/src/component/signup/ContactStep.tsx
@@ -1,19 +1,55 @@
 "use client";
 
-import { TextField } from '@heroui/react';
+import { Button, Input, Label, TextField } from '@heroui/react';
 import React from 'react';
 
 export default function ContactStep() {
 
     const [inputTel, setInputTel] = React.useState("");
-    [2]
+
+    const [address1, setAddress1] = React.useState("");
+    const [address2, setAddress2] = React.useState("");
+
+    const onPressSignUp = () => {
+
+    }
 
     return (
         <div className='flex flex-col gap-2'>
+            { /* Phone Part */}
             <TextField
-                type='tel'>
-
+                type='tel'
+                onChange={(e)=> {
+                    setInputTel(e)
+                }}>
+                <Label>Phone</Label>
+                <Input
+                    className="form-input" 
+                    value={inputTel}/>
             </TextField>
+            
+            {/* Address 1 Part*/}
+            <TextField
+                isReadOnly={true}>
+                <Label>Address 1</Label>
+                <div className='flex flex-row gap-2'>
+                    <Input className="w-[75%] form-input"/>
+                    <Button className="w-[25%] bg-yellow-500 text-black">
+                        Search
+                    </Button>
+                </div>  
+            </TextField>
+            
+            {/* Address 2 Part */}
+            <TextField>
+                <Label>Address 2(Optional)</Label>
+                <Input className="form-input" />
+            </TextField>
+            <Button 
+                className="w-full bg-yellow-500 text-black"
+                onPress={onPressSignUp}>
+                Sign Up
+            </Button>
         </div>
     )
 }
