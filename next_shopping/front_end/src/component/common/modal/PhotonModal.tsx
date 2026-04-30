13a4270fd8483671cf64df20bad02511b5a655a5
BYSooooo <avera0128@gmail.com>
2026-04-30 23:36:15 +0900

    [260430]In Dev Front end Sign Up

--- next_shopping/front_end/src/component/common/modal/PhotonModal.tsx
+++ next_shopping/front_end/src/component/common/modal/PhotonModal.tsx
@@ -3,21 +3,24 @@
 import React from 'react';
 
 import { useModalStore } from '@/zustand/useModalStore';
-import { Button, Input, Label, ListBox, Modal, TextField } from '@heroui/react';
+import { Avatar, Button, Description, Input, Label, ListBox, Modal, TextField } from '@heroui/react';
 import { useToastStore } from '@/zustand/useToastStore';
+import { BuildingOffice2Icon } from '@heroicons/react/24/outline';
 
 export default function PhotonModal() {
     const { closeModal } = useModalStore()
     const { openToast } = useToastStore()
 
     const [inputAddr, setInputAddr] = React.useState("");
+    const [resultList, setResultList] = React.useState([]);
 
     const onPressSearchAddr = async() => {
         if(inputAddr.length < 3) return;
         try {
-            const res = await fetch(`https://photon.komoot.io/api/?q=${encodeURIComponent(inputAddr)}&limit=5`)
+            const res = await fetch(`https://photon.komoot.io/api/?q=${encodeURIComponent(inputAddr)}&limit=20`)
             const result = await res.json();
             console.log(result)
+            setResultList(result.features)
         } catch(error) {
             console.log(error)
         } 
@@ -43,8 +46,27 @@ export default function PhotonModal() {
                             </Button>
                         </div>
                     </TextField>
-                    <ListBox selectionMode='single'>
+                    <ListBox 
+                        selectionMode='single' >
+                        { resultList.map((item)=> {
+                            const itemKey = item.properties.osm_id;
+                            return (
+                                <div>
+                                    <ListBox.Item key={itemKey}>
+                                        <Avatar size="sm">
+                                            <BuildingOffice2Icon className='w-5 h-5'/>
+                                        </Avatar>
+                                        <div className='flex flex-col'>
+                                            <Label>{item.properties.name}</Label>
+                                            <Description>
+                                                { item.properties.state}
+                                            </Description>
+                                        </div>
+                                    </ListBox.Item>
+                                </div>
+                                )
 
+                        })}
                     </ListBox>
                 </Modal.Body>
         </Modal.Dialog>
