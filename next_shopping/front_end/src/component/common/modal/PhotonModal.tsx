ced44aaaa08bd2fac944ad253f9c58c89f45323f
BYSooooo <avera0128@gmail.com>
2026-05-01 23:58:34 +0900

    [260501]In Dev Front End Sign Up Address Search

--- next_shopping/front_end/src/component/common/modal/PhotonModal.tsx
+++ next_shopping/front_end/src/component/common/modal/PhotonModal.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 import { useModalStore } from '@/zustand/useModalStore';
-import { Avatar, Button, Description, Input, Label, ListBox, Modal, TextField } from '@heroui/react';
+import { Avatar, Button, Description, Input, Label, ListBox, Modal, ScrollShadow, TextField } from '@heroui/react';
 import { useToastStore } from '@/zustand/useToastStore';
 import { BuildingOffice2Icon } from '@heroicons/react/24/outline';
 
@@ -33,25 +33,31 @@ export default function PhotonModal() {
                     Search Address
                 </Modal.Header>
                 <Modal.Body className="flex flex-col gap-3">
-                    <TextField
-                        onChange={(e)=> setInputAddr(e)}>
-                        <Label>keyword</Label>
-                        <div className='flex flex-row gap-2'>
-                            <Input
-                                className="form-input w-[75%]"/>
-                            <Button
-                                onPress={onPressSearchAddr} 
-                                className="w-[25%] bg-yellow-500 text-black">
-                                Search
-                            </Button>
-                        </div>
-                    </TextField>
-                    <ListBox 
-                        selectionMode='single' >
-                        { resultList.map((item)=> {
-                            const itemKey = item.properties.osm_id;
-                            return (
-                                <div>
+                    <div className='flex-none'>
+                        <TextField
+                            onChange={(e)=> setInputAddr(e)}>
+                            <Label>keyword</Label>
+                            <div className='flex flex-row gap-2'>
+                                <Input
+                                    className="form-input w-[75%]"/>
+                                <Button
+                                    onPress={onPressSearchAddr} 
+                                    className="w-[25%] bg-yellow-500 text-black">
+                                    Search
+                                </Button>
+                            </div>
+                        </TextField>
+                    </div>
+                    <div className='flex-1 overflow-hidden flex flex-col mt-2'>
+                        <Label className='text-xs text-gray-500'>Results</Label>
+                    </div>
+                    <ScrollShadow>
+
+                        <ListBox 
+                            selectionMode='single' >
+                            { resultList.map((item)=> {
+                                const itemKey = item.properties.osm_id;
+                                return (
                                     <ListBox.Item key={itemKey}>
                                         <Avatar size="sm">
                                             <BuildingOffice2Icon className='w-5 h-5'/>
@@ -63,11 +69,11 @@ export default function PhotonModal() {
                                             </Description>
                                         </div>
                                     </ListBox.Item>
-                                </div>
-                                )
+                                    )
 
-                        })}
-                    </ListBox>
+                            })}
+                        </ListBox>
+                    </ScrollShadow>
                 </Modal.Body>
         </Modal.Dialog>
     )
