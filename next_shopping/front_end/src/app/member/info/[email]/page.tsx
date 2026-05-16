7b7e68dd2a6a9457b4e42aa26a196aa014721ede
BYSooooo <avera0128@gmail.com>
2026-05-16 23:59:11 +0900

    [260516]In Dev Inform Page

---
+++ next_shopping/front_end/src/app/member/info/[email]/page.tsx
@@ -0,0 +1,18 @@
+import { Card } from "@heroui/react";
+
+
+export default function Page() {
+    
+    
+
+    return (
+        <div className="inner-container flex items-center h-screen justify-center flex-row">
+            <Card>
+                <Card.Header>
+                    This is Info Page
+                </Card.Header>
+            </Card>
+
+        </div>
+    )
+}
