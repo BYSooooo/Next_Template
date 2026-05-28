f43c87a2076b8a8c21f22e4af667db4fc02a1bfc
BYSooooo <avera0128@gmail.com>
2026-05-28 23:12:44 +0900

    [260528]In Dev Front End Auth Control in Session

--- next_shopping/front_end/src/component/provider/AuthProvider.tsx
+++ next_shopping/front_end/src/component/provider/AuthProvider.tsx
@@ -1,6 +1,73 @@
 "use client";
 
 import React from 'react';
-export default function AuthProvider({ children } : {children : React.ReactNode}) {
 
+import { usePathname, useRouter } from 'next/navigation';
+import { useAuthStore } from '@/zustand/useAuthStore';
+import { useToastStore } from '@/zustand/useToastStore';
+
+// Public Path - open to anybody
+const PUBLIC_ROUTE = [
+    "/",
+    "/member/signup",
+    "/login"
+]
+
+export default function AuthProvider({ children } : {children : React.ReactNode}) {
+    const pathname = usePathname();
+    const router = useRouter();
+    const { isSignIn, user } = useAuthStore();
+    const { openToast } = useToastStore();
+    const [ isInitialized, setIsInitialized ] = React.useState(false)
+
+    React.useEffect(()=> {
+        const verifySessionWithServer = async()=> {
+            try {
+                const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/me`, {
+                    method : 'GET',
+                    credentials : 'include'
+                });
+
+                if(!res.ok) {
+                    useAuthStore.getState().signOut()
+                }  else {
+                    console.log(res.statusText)
+                }
+            } catch(error) {
+                //...
+            } finally {
+                setIsInitialized(true)
+            }
+        }
+        verifySessionWithServer()
+    },[])
+
+    // Turn on Flag of initialized.
+    // Role of waiting for Client Side Rendering
+    React.useEffect(()=> {
+        setIsInitialized(true)
+    },[])
+
+    React.useEffect(()=> {
+        if(!isInitialized) return;
+
+        // Check Public route or not
+        const isProtectedRoute = !PUBLIC_ROUTE.includes(pathname);
+        if(isProtectedRoute && !isSignIn) {
+            openToast({
+                title : "Redirect to Main Page",
+                description : "You need to log in to access this page.",
+                variant : 'warning'
+            })
+            router.push("/")
+        }
+    },[pathname, isSignIn, isInitialized, router])
+
+    if(!isInitialized) {
+
+    }
+
+
+
+    return <>{children}</>
 }
