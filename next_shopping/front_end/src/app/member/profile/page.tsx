c0b2ff488894119242c0e147ed1dd8403664be5a
BYSooooo <avera0128@gmail.com>
2026-06-17 23:53:27 +0900

    [260617]In Dev Front End Profile Page

--- next_shopping/front_end/src/app/member/profile/page.tsx
+++ next_shopping/front_end/src/app/member/profile/page.tsx
@@ -2,18 +2,20 @@
 
 import React from 'react';
 
-import { Avatar, Card, Separator } from "@heroui/react";
+import { Avatar, Card, Description, Label, ListBox, Separator, Surface } from "@heroui/react";
 import { useAuthStore } from '@/zustand/useAuthStore';
 import { useRouter } from 'next/navigation';
 import AvatarCard from '@/component/profile/AvatarCard';
 import InfoOneCard from '@/component/profile/InfoOneCard';
 import AddressCard from '@/component/profile/AddressCard';
+import { EnvelopeIcon, HashtagIcon, KeyIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 
 
 export default function Page() {
     const router = useRouter();
     const { user } = useAuthStore();
-    
+
+
     return (
         <div className="inner-container flex items-center h-screen justify-center flex-row">
             <Card className='bg-gray-100 rounded-lg w-2/3'>
@@ -23,9 +25,57 @@ export default function Page() {
                     </p>
                 </Card.Header>
                 <Card.Content className='flex flex-row gap-4'>
-                    <AvatarCard />
-                    <InfoOneCard />
-                    <AddressCard />
+                    <Surface 
+                        variant='transparent'
+                        className='rounded-xl'>
+                        <ListBox selectionMode='single' className='w-fit'>
+                            <ListBox.Item>
+                                <Avatar className='p-1'>
+                                    <UserCircleIcon className='w-full'/>
+                                </Avatar>
+                                <div className='flex flex-col'>
+                                    <Label>Avatar</Label>
+                                    <Description>
+                                        Change, Remove Avatar
+                                    </Description>
+                                </div>
+                            </ListBox.Item>
+                            <ListBox.Item>
+                                <Avatar className='p-1'>
+                                    <EnvelopeIcon className='w-full'/>
+                                </Avatar>
+                                <div className='flex flex-col'>
+                                    <Label>Email</Label>
+                                    <Description>
+                                        Change, Verify Email Address
+                                    </Description>
+                                </div>
+                            </ListBox.Item>
+                            <ListBox.Item>
+                                <Avatar className='p-1'>
+                                    <HashtagIcon className='w-full'/>
+                                </Avatar>
+                                <div className='flex flex-col'>
+                                    <Label>Nickname</Label>
+                                    <Description>
+                                        Check Nickname
+                                    </Description>
+                                </div>
+                            </ListBox.Item>
+                            <ListBox.Item>
+                                <Avatar className='p-1'>
+                                    <KeyIcon className='w-full'/>
+                                </Avatar>
+                                <div className='flex flex-col'>
+                                    <Label>Password</Label>
+                                    <Description>
+                                        Change Password
+                                    </Description>
+                                </div>
+                            </ListBox.Item>
+                        </ListBox>
+
+                    </Surface>
                 </Card.Content>
                 <Card.Footer>
                     
